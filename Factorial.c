422394ba43435eebe0d7dbfe4818b5e213d1cab6
Prateek16vish <99818384+Prateek16vish@users.noreply.github.com>
2023-10-25 13:50:11 +0530

    Update Factorial.c

--- Factorial.c
+++ Factorial.c
@@ -4,10 +4,9 @@ void main() {
 int n,i,m=1;
 printf("Enter number to find factorial of n :");
 scanf("%d",&n);
-printf("Factorial is:");
 for(i=n;i>=1;i--){
     m=m*i;
 }
-printf("%d \n",m);
+printf("Factorial is: %d \n",m);
     getch();
 }
